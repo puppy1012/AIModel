8da40a13a42dec32e5e76771200b7519d5427e3f
youda <yudahyeon2@naver.com>
2023-05-29 18:22:19 +0900

    noise 덜 있는 음성 출력하는 테스트 코드

--- batch1_conversion.py
+++ batch1_conversion.py
@@ -19,14 +19,11 @@ import matplotlib.pyplot as plt
 #GAN에 필요한 parameters
 args = argparse.Namespace(
     attr = 'C:/Users/ICT/Desktop/youda/AI_TeamRepo/AIModel/RelGAN/attributes.yaml',
-    data = 'C:/Users/ICT/Desktop/youda/AI_TeamRepo/voiceDatsaetv6',
-    dataset = 'voiceDataset',
-    load = '/C:/Users/ICT/Desktop/youda/AI_TeamRepo/AIModel/model/middletosad.19999.499.pth',
+    load = 'C:/Users/ICT/Desktop/youda/AI_TeamRepo/AIModel/RelGAN/model/middletosad.19999.pth',
     save = 'model/test.pth',
-    steps = 4000,
-    start_step = 0,
-    name = 'voice Conversion',
-    gpu = False,
+    path = "C:/Users/ICT/Desktop/youda/AI_TeamRepo/test", #감정 변환된 wav파일을 저장하고 싶은 path
+    steps = 40000, #GAN에서 필요함
+    gpu = True,
     multi_gpu= False,
     lr = 5e-5,
     b1 = 0.5,
@@ -36,7 +33,6 @@ args = argparse.Namespace(
     repeat_G = 6,
     batch_size = 5,
     sample_size = 2,
-    epochs = 5,
     l1 = 10.0,
     l2 = 10.0,
     l3 = 1.0,
@@ -45,12 +41,7 @@ args = argparse.Namespace(
     zero_consistency = True,
     cycle_consistency = True,
     interpolation_regularize = True,
-    orthogonal_regularize = True,
-
-    log_interval = 1000,
-    sample_interval = 1000,
-    save_interval = 10000,
-    selected_attributes = ['sad']
+    orthogonal_regularize = True
 )
 
 args.selected_attributes = yaml.full_load(open(args.attr, 'r', encoding='utf-8'))
@@ -60,11 +51,6 @@ assert type(selected_attributes) is list
 #=========================변경할 감정 입력하기==========================
 #middle -> sad로 변경
 interpolating_attributes = ['middle']
-
-test_attributes = [
-    ('angry', 0), ('embarrassed', 0), ('middle', 0), 
-    ('pleasure', 0), ('sad', 1)
-]
 
 #========================변경할 데이터들 입력하기========================
 img = np.load('C:/Users/ICT/Desktop/youda/AI_TeamRepo/testDatasetv15/middle/100.npy').tolist()
@@ -76,43 +62,39 @@ attr = torch.tensor(attr, dtype=torch.float).to('cuda')
 
 #kobert에서 받아올 감정 리스트
 kobert_attr = [1, 0, 0, 0, 0]
-kobert_attr = torch.tensor(kobert_attr, dtype=torch.float).to('cuda')
 
 #=============================모델 가져오기=============================
 gan = GAN(args)
 gan.to('cuda')
 g, d, d_critic = gan.summary()
 
-load_file = 'C:/Users/ICT/Desktop/youda/AI_TeamRepo/AIModel/RelGAN/model/batch_1_weight.19999.pth'
+load_file = args.load
 if load_file is not None:
     gan.load(load_file)
 gan.eval()
 
 #=============================test 시작하기=============================
-attribute = kobert_attr - attr #relative attribute 만들기
-attribute = attribute.repeat(img.size(0), 1)
+attribute = torch.tensor(kobert_attr, dtype=torch.float).repeat(img.size(0),1)
+attribute = attribute.cuda()
 img0 = img.detach().unsqueeze(1).cpu() #음성 후처리위한 데이터?
-gen = []
+gen = [] #generate한 데이터들 모아둘 리스트
 gen.append(img0)
 #Generate하기
 with torch.no_grad():
     conversion = gan.G(img, attribute).detach()
 gen[-1] = torch.cat([gen[-1], conversion.unsqueeze(1).cpu()], dim=1) #generate list에 쌓기
 gen = torch.cat(gen)
-# print(gen.size(0), gen.size(1), gen.size(2), gen.size(3), gen.size(4))
+print("00",gen.size(0), gen.size(1), gen.size(2), gen.size(3), gen.size(4))
 
-gen = gen.view(gen.size(0)*gen.size(1), gen.size(2), gen.size(3), gen.size(4))
-conversion_test = make_grid(inv_normalize(conversion), nrow=1)
-conversion_test = conversion_test.cpu()
-print("사이즈: ",conversion_test.size())
-for i in range(conversion_test.size(0)):
-    testSet = np.split(conversion_test, 3)
-    print(f"{i}번째 사이즈: ", np.shape(testSet[i]))
+gen = gen.view(gen.size(0)*gen.size(1)*gen.size(2), gen.size(3), gen.size(4))
+print("사이즈: ",gen.size())
+for i in range(gen.size(0)):
+    testSet = np.split(gen, 2)
+    print(f"{i}번째 사이즈: ", np.shape(testSet[i])) #0번쨰만 들어가게 수정하기'''
 
 # np.save('C:/Users/ICT/Desktop/youda/AI_TeamRepo/test/test_4', conversion_test)
 
 #=============================numpy data 음성으로 변환하기=============================
 from inv_mel import npytomp4
-for i in range(conversion_test.size(0)):
-    npytomp4(npy=testSet[i], path=f"C:/Users/ICT/Desktop/youda/AI_TeamRepo/test/test_{i}.wav" )
-# npytomp4(conversion_test,path="C:/Users/ICT/Desktop/youda/AI_TeamRepo/test/test_1.wav")
+for i in range(gen.size(0)):
+    npytomp4(npy=testSet[i], path=os.path.join(args.path,f"test3_{i}.wav"))
